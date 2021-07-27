b8db06726b92ea2202b6d7eb7325755bc477b94b
Pratyusha <44306139+gudiausha@users.noreply.github.com>
2021-07-27 23:39:52 +0530

    Update time_to_buy.py

--- time_to_buy.py
+++ time_to_buy.py
@@ -5,10 +5,14 @@ class Solution(object):
         :rtype: int
         """
         min_stock = min(prices)
-        if prices.index(min_stock) >= len(prices):
+        min_index = prices.index(min_stock)
+        #print(min_stock, prices.index(min_stock),len(prices))
+        if prices.index(min_stock) >= len(prices)-1:
+            print(prices.index(min_stock),len(prices))
             return 0
         else:
-            new_stock_price = prices[min_stock:]
+            new_stock_price = prices[min_index:]
+         #   print(new_stock_price)
             max_stock = max(new_stock_price)
             return (abs(min_stock - max_stock))
         
